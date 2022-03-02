0e71f3e09658e4bf1b81986580895d3d12a4a737
Alonso Rodriguez <ARR552@users.noreply.github.com>
2022-03-02 10:44:49 +0100

    EthermanI on the consumer side in the Synchronizer (#29)

---
+++ synchronizer/interfaces.go
@@ -0,0 +1,16 @@
+package synchronizer
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/hermeznetwork/hermez-bridge/etherman"
+)
+
+type localEtherMan interface {
+	GetBridgeInfoByBlockRange(ctx context.Context, fromBlock uint64, toBlock *uint64) ([]etherman.Block, map[common.Hash][]etherman.Order, error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+	BlockByNumber(ctx context.Context, blockNumber uint64) (*types.Block, error)
+}
