89906b5c024141c7dac208325ef8da87b820ff91
Cool Developer <cool199966@outlook.com>
2022-04-29 10:50:21 +0100

    update the docker image tag

--- scripts/cmd/dependencies.go
+++ scripts/cmd/dependencies.go
@@ -17,7 +17,7 @@ func updateDeps(ctx *cli.Context) error {
 
 	cfg := &dependencies.Config{
 		Images: &dependencies.ImagesConfig{
-			Names:          []string{"hermeznetwork/hermez-node-zkevm", "hermeznetwork/geth-zkevm-contracts", "hermeznetwork/hez-mock-prover"},
+			Names:          []string{"hermeznetwork/hermez-node-zkevm:develop", "hermeznetwork/geth-zkevm-contracts", "hermeznetwork/hez-mock-prover"},
 			TargetFilePath: filepath.Join(dir, "docker-compose.yml"),
 		},
 		PB: &dependencies.PBConfig{
