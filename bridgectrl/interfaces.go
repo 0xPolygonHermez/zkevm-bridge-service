6136b47a90146565afb525ef2ee1408706b8b6ec
Alonso <ARR551@protonmail.com>
2022-05-04 15:54:22 +0200

    linter

--- bridgectrl/interfaces.go
+++ bridgectrl/interfaces.go
@@ -3,8 +3,8 @@ package bridgectrl
 import (
 	"context"
 
-	"github.com/hermeznetwork/hermez-bridge/etherman"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/hermeznetwork/hermez-bridge/etherman"
 )
 
 // merkleTreeStore interface for the Merkle Tree
