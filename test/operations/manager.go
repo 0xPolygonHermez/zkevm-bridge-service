91975ed846e999e5ab0e16b69d3c29725fbac5c6
tclemos <thiago@iden3.com>
2022-08-04 13:55:05 -0300

    fix L2 account private key and set gas to deposit tx to avoid estimating

--- test/operations/manager.go
+++ test/operations/manager.go
@@ -62,7 +62,7 @@ var (
 	}
 	accHexPrivateKeys = map[NetworkSID]string{
 		L1: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-		L2: "0xdfd01798f92667dbf91df722434e8fbe96af0211d4d1b82bbbbc8f1def7a814f", //0xc949254d682d8c9ad5682521675b8f43b102aec4
+		L2: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 	}
 )
 
@@ -176,9 +176,13 @@ func (m *Manager) SendL2Deposit(ctx context.Context, tokenAddr common.Address, a
 ) error {
 	client := m.clients[L2]
 	auth, err := client.GetSigner(ctx, accHexPrivateKeys[L2])
+
 	if err != nil {
 		return err
 	}
+
+	auth.GasLimit = 10 ^ 9        // TODO remove when the executor is fixed to estimate gas
+	auth.GasPrice = big.NewInt(1) // TODO remove when the executor is fixed to estimate gas
 
 	lastBlockID, err := m.getLastBlockID(ctx)
 	if err != nil {
