89df29eb61e4f57a1a18e13e98c9ea8547a4e7ff
Cool Developer <kevin@polygon.technology>
2023-03-31 11:46:06 +0100

    fix migration

--- db/pgstorage/migrations/0002_test.go
+++ db/pgstorage/migrations/0002_test.go
@@ -3,7 +3,6 @@ package migrations_test
 import (
 	"database/sql"
 	"testing"
-	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
@@ -28,19 +27,7 @@ func (m migrationTest0002) InsertData(db *sql.DB) error {
 	}); err != nil {
 		return err
 	}
-	// Insert initial block, deposit and root
-	if _, err := db.Exec("INSERT INTO syncv2.block (block_num, block_hash, parent_hash, network_id, received_at) VALUES ($1, $2, $3, $4, $5)", 1, common.FromHex("0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5"),
-		common.FromHex("0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5"), 0,
-		time.Now()); err != nil {
-		return err
-	}
-	if _, err := db.Exec("INSERT INTO syncv2.deposit (leaf_type, network_id, orig_net, orig_addr, amount, dest_net, dest_addr, block_id, deposit_cnt, tx_hash, metadata) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
-		0, 0, 0, common.FromHex("0x0000000000000000000000000000000000000000"),
-		"1000000", 1, common.FromHex("0x6B175474E89094C44Da98b954EedeAC495271d0F"), 1, 0,
-		common.FromHex("0xa4bfa0908dc7b06d98da4309f859023d6947561bc19bc00d77f763dea1a0b9f5"),
-		[]byte{}); err != nil {
-		return err
-	}
+	// Insert initial root
 	if _, err := db.Exec("INSERT INTO mtv2.root (root, deposit_cnt, network) VALUES ($1, $2, $3)",
 		common.FromHex("0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5"), 0, 0); err != nil {
 		return err
@@ -48,28 +35,22 @@ func (m migrationTest0002) InsertData(db *sql.DB) error {
 	return nil
 }
 
+var indexes = []string{"root_network_idx", "deposit_idx", "block_idx", "root_idx", "exit_roots_idx"}
+
 func (m migrationTest0002) RunAssertsAfterMigrationUp(t *testing.T, db *sql.DB) {
-	// Insert a new Deposit
-	var depositID uint64
-	err := db.QueryRow("INSERT INTO sync.deposit (leaf_type, network_id, orig_net, orig_addr, amount, dest_net, dest_addr, block_id, deposit_cnt, tx_hash, metadata) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id",
-		0, 0, 0, common.FromHex("0x0000000000000000000000000000000000000000"),
-		"1000000", 1, common.FromHex("0x6B175474E89094C44Da98b954EedeAC495271d0F"), 1, 1,
-		common.FromHex("0xa4bfa0908dc7b06d98da4309f859023d6947561bc19bc00d77f763dea1a0b9f5"),
-		[]byte{}).Scan(&depositID)
-	assert.NoError(t, err)
 	// Insert a new root
-	const addRoot = "INSERT INTO mt.root (root, deposit_cnt, network, deposit_id) VALUES ($1, $2, $3, $4)"
-	_, err = db.Exec(addRoot, common.FromHex("0x5a2dce0a8a7f68bb74560f8f71837c2c2ebbcbf7fffb42ae1896f13f7c7479a0"), 1, 0, depositID)
+	const addRoot = "INSERT INTO mt.root (root, deposit_cnt, network) VALUES ($1, $2, $3) RETURNING id"
+	var rootID int
+	err := db.QueryRow(addRoot, common.FromHex("0x5a2dce0a8a7f68bb74560f8f71837c2c2ebbcbf7fffb42ae1896f13f7c7479a0"), 1, 0).Scan(&rootID)
 	assert.NoError(t, err)
+
 	// Insert a new node to the rht table
-	const addNode = "INSERT INTO mt.rht (key, value, deposit_id) VALUES ($1, $2, $3)"
+	const addNode = "INSERT INTO mt.rht (key, value, root_id) VALUES ($1, $2, $3)"
 	_, err = db.Exec(addNode, common.FromHex("0x5a2dce0a8a7f68bb74560f8f71837c2c2ebbcbf7fffb42ae1896f13f7c7479a0"), [][]byte{
 		common.FromHex("0xb4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30"),
 		common.FromHex("0x21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85"),
-	}, depositID)
+	}, rootID)
 	assert.NoError(t, err)
-
-	indexes := []string{"root_network_idx", "deposit_idx", "block_idx", "root_idx", "exit_roots_idx"}
 
 	// Check indexes adding
 	for _, idx := range indexes {
@@ -96,8 +77,6 @@ func (m migrationTest0002) RunAssertsAfterMigrationDown(t *testing.T, db *sql.DB
 	// Insert an new root to reproduce the duplicate key error
 	_, err = db.Exec(addOldRoot, common.FromHex("0xf4418588ed35a2458cffeb39b93d26f18d2ab13bdce6aee58e7b99359ec2dfd9"), 2, 0)
 	assert.NoError(t, err)
-
-	indexes := []string{"root_network_idx", "deposit_idx", "block_idx", "root_idx", "exit_roots_idx"}
 
 	// Check indexes removing
 	for _, idx := range indexes {
--- db/pgstorage/migrations/0003.sql
+++ db/pgstorage/migrations/0003.sql
@@ -1,22 +1,63 @@
 -- +migrate Down
-DROP INDEX IF EXISTS mt.rht_key_idx;
 
+ALTER TABLE mt.rht DROP COLUMN IF EXISTS deposit_id;
+ALTER TABLE mt.root DROP COLUMN IF EXISTS deposit_id;
+ALTER TABLE sync.deposit DROP COLUMN IF EXISTS id;
+
+ALTER TABLE sync.deposit ADD CONSTRAINT deposit_pkey PRIMARY KEY (network_id, deposit_cnt);
+
+DROP INDEX IF EXISTS mt.rht_key_idx;
 DROP TABLE IF EXISTS sync.monitored_txs;
 
-ALTER TABLE
-    sync.deposit DROP COLUMN ready_for_claim;
+ALTER TABLE sync.deposit DROP COLUMN ready_for_claim;
 
-ALTER TABLE
-    sync.block DROP CONSTRAINT block_hash_unique;
+ALTER TABLE sync.block DROP CONSTRAINT block_hash_unique;
 
-ALTER TABLE
-    mt.rht
-ADD
-    PRIMARY KEY (key);
+ALTER TABLE mt.root ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE mt.rht ADD COLUMN root_id BIGINT NOT NULL DEFAULT 1 CONSTRAINT rht_root_id_fkey REFERENCES mt.root (id) ON DELETE CASCADE;
+ALTER TABLE mt.rht ALTER COLUMN root_id DROP DEFAULT;
+
+-- +migrate StatementBegin
+DO $$
+DECLARE
+	rt RECORD;
+	pkey BYTEA;
+	pvalue BYTEA [];
+BEGIN
+	FOR rt IN SELECT * FROM mt.root 
+	LOOP
+		pkey = rt.root;
+		FOR i IN reverse 31..0
+		LOOP
+			UPDATE mt.rht SET root_id = rt.id WHERE key = pkey RETURNING value INTO pvalue;
+			
+			IF rt.deposit_cnt & (1 << i) > 0 THEN
+				pkey = pvalue[2];
+			ELSE
+				pkey = pvalue[1];
+			END IF;
+		END LOOP;
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+-- +migrate StatementEnd
+
+UPDATE mt.root SET deposit_cnt = deposit_cnt + 1;
 
 -- +migrate Up
-ALTER TABLE
-    mt.rht DROP CONSTRAINT IF EXISTS rht_pkey;
+
+ALTER TABLE mt.rht DROP COLUMN IF EXISTS root_id;
+ALTER TABLE mt.root DROP COLUMN IF EXISTS id;
+
+ALTER TABLE sync.deposit DROP CONSTRAINT IF EXISTS deposit_pkey;
+ALTER TABLE sync.deposit ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE mt.root ADD COLUMN deposit_id BIGINT NOT NULL DEFAULT 1 CONSTRAINT root_deposit_id_fkey REFERENCES sync.deposit (id) ON DELETE CASCADE;
+ALTER TABLE mt.root ALTER COLUMN deposit_id DROP DEFAULT;
+UPDATE mt.root AS r SET deposit_id = d.id FROM sync.deposit AS d WHERE d.deposit_cnt = r.deposit_cnt AND d.network_id = r.network;
+
+ALTER TABLE mt.rht ADD COLUMN deposit_id BIGINT NOT NULL DEFAULT 1 CONSTRAINT rht_deposit_id_fkey REFERENCES sync.deposit (id) ON DELETE CASCADE;
+ALTER TABLE mt.rht ALTER COLUMN deposit_id DROP DEFAULT;
 
 ALTER TABLE
     sync.deposit
@@ -43,7 +84,8 @@ CREATE TABLE sync.monitored_txs (
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
--- +migrate StatementBegin
+UPDATE mt.root SET deposit_cnt = deposit_cnt - 1;
+
 UPDATE
     sync.deposit
 SET
@@ -98,6 +140,31 @@ WHERE
     )
     AND network_id != 0;
 
+
+-- +migrate StatementBegin
+DO $$
+DECLARE
+	rt RECORD;
+	pkey BYTEA;
+	pvalue BYTEA [];
+BEGIN
+	FOR rt IN SELECT * FROM mt.root 
+	LOOP
+		pkey = rt.root;
+		FOR i IN reverse 31..0
+		LOOP
+			UPDATE mt.rht SET deposit_id = rt.deposit_id WHERE key = pkey RETURNING value INTO pvalue;
+			
+			IF rt.deposit_cnt & (1 << i) > 0 THEN
+				pkey = pvalue[2];
+			ELSE
+				pkey = pvalue[1];
+			END IF;
+		END LOOP;
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 -- +migrate StatementEnd
+
 -- Create indexes
 CREATE INDEX IF NOT EXISTS rht_key_idx ON mt.rht(key);
