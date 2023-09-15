3bfa7e408d42d830d442f8cb88605d05efa7efc2
bros <arnaubennassar5@gmail.com>
2023-09-15 08:12:48 +0000

    L2 => L1 flow working

--- test/validium/customnativetoke_test.go
+++ test/validium/customnativetoke_test.go
@@ -57,16 +57,10 @@ func TestCustomNativeToken(t *testing.T) {
 	}
 	opsman, err := operations.NewManager(ctx, opsCfg)
 	require.NoError(t, err)
-	/*
-		1. Bridge from L1 to L2.
-		2. Bridge from L2 to L1
-		3. Claim the deposits in both layers
-	*/
 	// Check initial globalExitRoot.
 	globalExitRootSMC, err := opsman.GetCurrentGlobalExitRootFromSmc(ctx)
 	require.NoError(t, err)
 	t.Logf("initial globalExitRootSMC: %+v,", globalExitRootSMC)
-	// Send L1 deposit
 	var destNetwork uint32 = 1
 	amount1 := new(big.Int).SetUint64(1000000)
 	nativeTokenL1Addr := common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3")
@@ -74,7 +68,8 @@ func TestCustomNativeToken(t *testing.T) {
 	require.NoError(t, err)
 	l1MaticSC, err := ERC20.NewERC20(nativeTokenL1Addr, clientL1)
 	require.NoError(t, err)
-	// Send MATIC to the random account
+
+	// Send MATIC to the L1 account
 	authL1WithFunds, err := validiumOperations.GetAuth(validiumOperations.DefaultSequencerPrivateKey, validiumOperations.DefaultL1ChainID)
 	require.NoError(t, err)
 	authL1, err := validiumOperations.GetAuth("0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a", validiumOperations.DefaultL1ChainID)
@@ -91,36 +86,65 @@ func TestCustomNativeToken(t *testing.T) {
 	require.NoError(t, err)
 	require.GreaterOrEqual(t, balance.Int64(), amount1.Int64())
 
+	// Check that recevier doesnt have funds before bridge
+	destAddr := common.HexToAddress("0xc949254d682d8c9ad5682521675b8f43b102aec4")
+	balance, err = opsman.CheckAccountBalance(ctx, operations.L2, &destAddr)
+	require.NoError(t, err)
+	require.Equal(t, balance.Int64(), int64(0))
+
+	// Send L1 deposit: MATIC on L1 => Native token on L2
 	bridgeAddr := common.HexToAddress("0x4C739372258826995C302CD655beE12689B97d3F")
 	client, err := utils.NewClient(ctx, "http://localhost:8545", bridgeAddr)
 	require.NoError(t, err)
 	err = client.ApproveERC20(ctx, nativeTokenL1Addr, bridgeAddr, amount1, authL1)
 	require.NoError(t, err)
-
-	destAddr := common.HexToAddress("0xc949254d682d8c9ad5682521675b8f43b102aec4")
-	// Check that recevier doesnt have funds beforedeposit
-	balance, err = opsman.CheckAccountBalance(ctx, operations.L2, &destAddr)
-	require.NoError(t, err)
-	require.Equal(t, balance.Int64(), int64(0))
-	// First deposit
 	err = opsman.SendL1Deposit(ctx, nativeTokenL1Addr, amount1, destNetwork, &destAddr)
 	require.NoError(t, err)
-	// Get Bridge Info By DestAddr
 	deposits, err := opsman.GetBridgeInfoByDestAddr(ctx, &destAddr)
 	require.NoError(t, err)
 	t.Log("Deposit: ", deposits[0])
 	t.Log("Before getClaimData: ", deposits[0].NetworkId, deposits[0].DepositCnt)
-	// Check the claim tx
 	err = opsman.CheckL2Claim(ctx, uint(deposits[0].DestNet), uint(deposits[0].DepositCnt))
 	require.NoError(t, err)
 
-	// time.Sleep(3 * time.Second) // wait for sync token_wrapped event
-	// tokenWrapped, err := opsman.GetTokenWrapped(ctx, 0, nativeTokenL1Addr, false)
-	// require.NoError(t, err)
-	// balance2, err := opsman.CheckAccountTokenBalance(ctx, "l2", tokenWrapped.WrappedTokenAddress, &destAddr)
-	// require.NoError(t, err)
-	// t.Log("Init account balance l2: ", balance2)
+	// Check the balance of native token on L2
 	balance, err = opsman.CheckAccountBalance(ctx, operations.L2, &destAddr)
 	require.NoError(t, err)
 	require.Equal(t, balance.Int64(), amount1.Int64())
+
+	// Check L1 funds of MATIC before L2 => L1 bridge
+	balance, err = opsman.CheckAccountTokenBalance(ctx, operations.L1, nativeTokenL1Addr, &destAddr)
+	require.NoError(t, err)
+	require.Equal(t, int64(0), balance.Int64())
+
+	// Send L2 Deposit: Native token on L2 => MATIC L1
+	destNetwork = 0
+	l2Balance, err := opsman.CheckAccountBalance(ctx, operations.L2, &bridgeAddr)
+	require.NoError(t, err)
+	t.Logf("L2 Bridge Balance: %v", l2Balance)
+	err = opsman.SendL2Deposit(ctx, common.Address{}, amount1, destNetwork, &destAddr)
+	require.NoError(t, err)
+	l2Balance, err = opsman.CheckAccountBalance(ctx, operations.L2, &bridgeAddr)
+	require.NoError(t, err)
+	t.Logf("L2 Bridge Balance: %v", l2Balance)
+
+	// Get Bridge Info By DestAddr
+	deposits, err = opsman.GetBridgeInfoByDestAddr(ctx, &destAddr)
+	require.NoError(t, err)
+	t.Log("Deposit 2: ", deposits[0])
+	// Get the claim data
+	smtProof, globaExitRoot, err := opsman.GetClaimData(ctx, uint(deposits[0].NetworkId), uint(deposits[0].DepositCnt))
+	require.NoError(t, err)
+	// Claim funds in L1
+	err = opsman.SendL1Claim(ctx, deposits[0], smtProof, globaExitRoot)
+	require.NoError(t, err)
+
+	// Check L1 funds to see if the amount has been increased (MATIC)
+	balance, err = opsman.CheckAccountTokenBalance(ctx, operations.L1, nativeTokenL1Addr, &destAddr)
+	require.NoError(t, err)
+	require.Equal(t, amount1, balance)
+	// Check L2 funds to see that the amount has been reduced (Native coin)
+	balance, err = opsman.CheckAccountBalance(ctx, operations.L2, &destAddr)
+	require.NoError(t, err)
+	require.Equal(t, int64(0), balance.Int64())
 }
