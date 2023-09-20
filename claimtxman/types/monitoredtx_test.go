2cae66f34e263aa6b9235c29f5e498d78bd2c992
Alonso <ARR551@protonmail.com>
2023-09-20 16:39:31 +0200

    fix test

--- claimtxman/types/monitoredtx_test.go
+++ claimtxman/types/monitoredtx_test.go
@@ -20,20 +20,24 @@ func TestHistoryHashSlice(t *testing.T) {
 	err := mTx.AddHistory(tx1)
 	require.NoError(t, err)
 	history := mTx.HistoryHashSlice()
-	for i := range history {
-		t.Logf("history %d: %s", i, common.Bytes2Hex(history[i]))
-		assert.Equal(t, txs[i].Hash(), common.BytesToHash(history[i]))
-		t.Log("TEST1: ", txs[i].Hash(), common.BytesToHash(history[i]))
-	}
+	t.Logf("history: %s", common.Bytes2Hex(history[0]))
+	assert.Equal(t, txs[0].Hash(), common.BytesToHash(history[0]))
+	t.Log("TEST1: ", txs[0].Hash(), common.BytesToHash(history[0]))
 
 	err = mTx.AddHistory(tx2)
 	require.NoError(t, err)
 	history = mTx.HistoryHashSlice()
+	var eq []bool
 	for i := range history {
 		t.Logf("history %d: %s", i, common.Bytes2Hex(history[i]))
-		assert.Equal(t, txs[i].Hash(), common.BytesToHash(history[i]))
-		t.Log("TEST2: ", txs[i].Hash(), common.BytesToHash(history[i]))
+		for j := range txs {
+			if common.BytesToHash(history[i]) == txs[j].Hash(){
+				eq = append(eq, true)
+				t.Log("TEST2: ", txs[j].Hash(), common.BytesToHash(history[i]))
+			}
+		}
 	}
+	assert.Equal(t, 2, len(eq))
 
 	mTx.RemoveHistory(tx1)
 	history = mTx.HistoryHashSlice()
