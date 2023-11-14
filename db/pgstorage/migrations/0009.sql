fd84379e2a9df7564a9506925dd16177b978e6c4
trunghai95 <bthai.1995@gmail.com>
2023-11-14 13:00:16 +0800

    Fix SQL

--- db/pgstorage/migrations/0009.sql
+++ db/pgstorage/migrations/0009.sql
@@ -4,4 +4,4 @@ ALTER TABLE sync.deposit DROP COLUMN IF EXISTS ready_time;
 
 -- +migrate Up
 
-ALTER TABLE sync.deposit ADD COLUMN ready_time TIMESTAMP WITH TIME ZONE;
+ALTER TABLE sync.deposit ADD COLUMN IF NOT EXISTS ready_time TIMESTAMP WITH TIME ZONE;
