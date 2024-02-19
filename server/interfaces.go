edbac37e602722a1e8b4bb19da7f187ae897f91e
bros <arnaubennassar5@gmail.com>
2024-02-19 16:48:56 +0000

    Fix wrong relation of claim_tx_hash

--- server/interfaces.go
+++ server/interfaces.go
@@ -13,7 +13,7 @@ type bridgeServiceStorage interface {
 	GetRoot(ctx context.Context, depositCnt uint, network uint, dbTx pgx.Tx) ([]byte, error)
 	GetDepositCountByRoot(ctx context.Context, root []byte, network uint8, dbTx pgx.Tx) (uint, error)
 	GetLatestExitRoot(ctx context.Context, isRollup bool, dbTx pgx.Tx) (*etherman.GlobalExitRoot, error)
-	GetClaim(ctx context.Context, index uint, networkID uint, dbTx pgx.Tx) (*etherman.Claim, error)
+	GetClaim(ctx context.Context, index uint, originNetworkID, destNetworkID uint, dbTx pgx.Tx) (*etherman.Claim, error)
 	GetClaims(ctx context.Context, destAddr string, limit uint, offset uint, dbTx pgx.Tx) ([]*etherman.Claim, error)
 	GetClaimCount(ctx context.Context, destAddr string, dbTx pgx.Tx) (uint64, error)
 	GetDeposit(ctx context.Context, depositCnt uint, networkID uint, dbTx pgx.Tx) (*etherman.Deposit, error)
