21cf8cd7bdeb34d03e82cb0683844d133edd9b31
bros <arnaubennassar5@gmail.com>
2024-03-06 09:36:59 +0000

    network_id is bigint

--- db/pgstorage/migrations/0012.sql
+++ db/pgstorage/migrations/0012.sql
@@ -11,9 +11,6 @@ ALTER COLUMN network_id TYPE BIGINT;
 ALTER TABLE sync.token_wrapped
 ALTER COLUMN network_id TYPE BIGINT;
 
-ALTER TABLE mt.root
-ALTER COLUMN network_id TYPE BIGINT;
-
 -- +migrate Down
 ALTER TABLE sync.claim
 ALTER COLUMN network_id TYPE INTEGER;
@@ -25,7 +22,4 @@ ALTER TABLE sync.block
 ALTER COLUMN network_id TYPE INTEGER;
 
 ALTER TABLE sync.token_wrapped
-ALTER COLUMN network_id TYPE INTEGER;
-
-ALTER TABLE mt.root
 ALTER COLUMN network_id TYPE INTEGER;
