7001828945c719d8234d8219622718bdfab777e3
trunghai95 <bthai.1995@gmail.com>
2024-04-03 12:39:37 +0800

    Update sql

--- db/pgstorage/migrations/1003.sql
+++ db/pgstorage/migrations/1003.sql
@@ -4,4 +4,4 @@ ALTER TABLE sync.deposit DROP COLUMN IF EXISTS dest_contract_addr;
 
 -- +migrate Up
 
-ALTER TABLE sync.deposit ADD COLUMN IF NOT EXISTS dest_contract_addr BYTEA;
+ALTER TABLE sync.deposit ADD COLUMN IF NOT EXISTS dest_contract_addr BYTEA NOT NULL DEFAULT '\x0000000000000000000000000000000000000000';
